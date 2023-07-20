fd800713e71d22f15a909df8bf5f18c915950ed2
Lee Pierson <awesomehenge@gmail.com>
2023-07-20 14:28:45 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
+import SelectedBeast from "./components/SelectedBeast/SelectedBeast";
+
+import data from "./data.json";
+import { useState } from "react";
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const [modalContent, setModalContent] = useState({});
+  // const [beastData, setBeastData] = useState(data);
+
+  function handleModal(beast) {
+    setModal(!modal);
+    setModalContent(beast);
+  }
+
+  function closeModal() {
+    setModal(!modal);
+    setModalContent({});
+  }
+
   return (
     <div className="App">
-     
+      <Header />
+      <Main data={data} handleModal={handleModal} />
+      {modal && <SelectedBeast modalContent={modalContent} closeModal={closeModal} />}
+      <Footer />
     </div>
   );
 }
